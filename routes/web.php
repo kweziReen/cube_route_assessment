d1aa9e406b69a12c6e720b3fd86369296dda3363
kweziReen <kwezi.mvula@gmail.com>
2023-06-29 23:58:07 +0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Livewire\Home;
+use App\Http\Livewire\Categories\Show;
+use App\Http\Controllers\AuthController;
+use App\Http\Livewire\Authentication\Login;
+use App\Http\Livewire\Authentication\Register;
+use App\Http\Livewire\Categories\Show as Category;
+use App\Http\Livewire\Categories\Index as Categories;
+use App\Http\Livewire\Products\Show as Product;
+use App\Http\Livewire\Products\Index as Products;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +24,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', Login::class);
+Route::get('login', Login::class)->name('login');
+Route::get('logout', [AuthController::class, 'logOut'])->name('logout');
+Route::get('register', Register::class)->name('register');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', Home::class)->name('home');
+    Route::get('category/{id}', Category::class);
+    Route::get('categories', Categories::class)->name('categories');
+    Route::get('product/{id}', Product::class);
+    Route::get('products', Products::class);
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
